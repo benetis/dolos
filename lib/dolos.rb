3ef6283302bc7ed941548082b9489d0b2b79bacd
benetis <git@benetis.me>
2023-08-13 16:51:18 +0300

    project: initial

---
+++ lib/dolos.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dolos/version"
+
+module Dolos
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/dolos_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Dolos do
+  it "has a version number" do
+    expect(Dolos::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
