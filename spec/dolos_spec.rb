9c1b24f06c8fe8ead0de76bd8bcccc19843254a3
benetis <git@benetis.me>
2023-08-15 13:17:24 +0300

    tests: move choice spec out of bundle

--- spec/dolos_spec.rb
+++ spec/dolos_spec.rb
@@ -13,59 +13,6 @@ RSpec.describe Dolos do
     end
   end
 
-  describe 'choice' do
-    it 'matches the first parser' do
-      parser = string('hello') | string('world')
-      result = parser.run('hello')
-
-      expect(result.success?).to be_truthy
-    end
-
-    it 'matches the second parser' do
-      parser = string('hello') | string('world')
-      result = parser.run('world')
-
-      expect(result.success?).to be_truthy
-    end
-
-    it 'returns failure if nothing matches' do
-      parser = string('hello') | string('world')
-      result = parser.run('!')
-
-      expect(result.failure?).to be_truthy
-    end
-
-    it 'handles failing parser before success and continues' do
-      parser = (string('hello') | string('world') | string('!')) >> string(" the ") >> string('end') | string('beginning')
-      result = parser.run('! the beginning')
-
-      expect(result.success?).to be_truthy
-    end
-
-    context 'captures' do
-      it 'captures the result of the first parser' do
-        parser = string('hello').capture! | string('world')
-        result = parser.run('hello')
-
-        expect(result.captures).to eq(['hello'])
-      end
-
-      it 'captures the result of the second parser' do
-        parser = string('hello') | string('world').capture!
-        result = parser.run('world')
-
-        expect(result.captures).to eq(['world'])
-      end
-
-      it 'captures groups' do
-        parser = (string('hello') | string('world')).capture!
-        result = parser.run('world')
-
-        expect(result.captures).to eq(['world'])
-      end
-    end
-  end
-
   describe 'associativity' do
     context '>>' do
       it 'is left associative' do
