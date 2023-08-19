5ea624e595b71d736571f32677063b2508258097
benetis <git@benetis.me>
2023-08-19 15:55:36 +0300

    clean: rename files to represent content

--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -3,55 +3,53 @@
 RSpec.describe Dolos do
   include Dolos
 
-  describe 'product' do
+  describe 'map' do
+    it 'maps over one parser' do
+      parser = string('hello').map_value { |value| value.upcase }
+      result = parser.run('hello')
 
-    context 'when success' do
-      it 'combines two parsers' do
-        parser = string('hello') >> string('world')
-        result = parser.run('helloworld')
-
-        expect(result.success?).to be_truthy
-      end
-
-      it 'combines three parsers' do
-        parser = string('hello') >> string('world') >> string('!')
-        result = parser.run('helloworld!')
-
-        expect(result.success?).to be_truthy
-      end
-
-      it 'combines four parsers' do
-        parser = string('hello') >> string('world') >> string('!') >> string('!')
-        result = parser.run('helloworld!!')
-
-        expect(result.success?).to be_truthy
-      end
-
-      it 'combines five parsers' do
-        parser = string('hello') >> string(' ') >> string('world') >> string(',') >> string(' and universe')
-        result = parser.run('hello world, and universe')
-
-        expect(result.success?).to be_truthy
-      end
+      expect(result.value).to eq('HELLO')
     end
 
-    context 'when failure' do
-      it 'tries to combine two parsers and returns failure' do
-        parser = string('hello') >> string('world')
-        result = parser.run('helloX')
+    it 'maps over two parsers' do
+      parser = (string('hello') >> string('world')).map_value { |value| value.map(&:upcase) }
+      result = parser.run('helloworld')
 
-        expect(result.failure?).to be_truthy
-      end
-
+      expect(result.value).to eq(['HELLO', 'WORLD'])
     end
 
-    context 'when returning a value' do
-      it 'returns parse value' do
-        parser = string('hello') >> string('X')
-        result = parser.run('helloX')
+    it 'maps over two parsers and uses them in a third' do
+      loud_hello = (string('hello') >> string('world')).map_value { |value| value.map(&:upcase) }
 
-        expect(result.value).to eq(['hello', 'X'])
-      end
+      parser = loud_hello >> string('!')
+
+      result = parser.run('helloworld!')
+      expect(result.value.flatten).to eq(['HELLO', 'WORLD', '!'])
+    end
+
+    it 'maps over parsers and converts them to ints' do
+      parser = (string("1") >> string("2") >> string("3")).capture!.flatten.map { |value| value.map(&:to_i) }
+
+      result = parser.run("123")
+      expect(result.captures).to eq([1, 2, 3])
+    end
+
+    it 'maps over groups and converts to ints' do
+      first = (string("1") >> string("2")).capture!.map { |value| value.map(&:to_i) }
+      second = (string("3") >> string("4")).capture!.map { |value| value.map(&:to_i) }
+      parser = (first >> second)
+
+      result = parser.run("1234")
+      expect(result.captures).to eq([1, 2, 3, 4])
+    end
+
+    it 'maps over groups to add and then multiply' do
+      first = (string("1") >> string("2")).capture!.map { |digit| digit.map(&:to_i).reduce(:+) } # 3
+      second = (string("3") >> string("4")).capture!.map { |digit| digit.map(&:to_i).reduce(:+) } # 7
+      parser = (first >> second).map { |value| value.reduce(:*) } # 21
+
+      result = parser.run("1234")
+      expect(result.captures).to eq(21)
     end
 
   end
